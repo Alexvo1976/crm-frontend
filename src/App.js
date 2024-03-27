8895032fa295832c6ef476e8b433d5b2242a9ed5
Alexvo1976 <106778131+Alexvo1976@users.noreply.github.com>
2024-03-27 00:42:08 -0500

    dashboard page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
+import { Dashboard } from './dashboard/Dashboard.page';
 import { DefaultLayout } from './layout/DefaultLayout';
 import { Entry } from './pages/entry/Entry.page';
 function App() {
   return (
     <div className="App">
       {/* <Entry /> */}
-      <DefaultLayout />
+      <DefaultLayout>
+        <Dashboard />
+      </DefaultLayout >
+
     </div>
   );
 }
